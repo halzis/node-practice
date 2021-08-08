3b0dc82806c9e74c59e9531b34f0c17a431cec12
halzis <halzis@kakao.com>
2021-08-08 16:42:53 +0900

    처음 저장소에 올림

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://halzis:dltk89@cluster0.x3oco.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+}).then(() => console.log('MongoDB Connected...'))
+    .catch(err => console.log('err'))
+
+
+app.get('/', (req, res) => {res.send('Hello World!')})
+
+app.listen(port, () => {console.log(`Example app listening at http://localhost:${port}`)})
