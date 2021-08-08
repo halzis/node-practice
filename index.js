00fc0b20ee5c9aa7ec69778764ffb9be669957fa
halzis <halzis@kakao.com>
2021-08-08 19:15:59 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,15 +1,39 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser')
+const { User } = require('./models/User')
 
+const config = require('./config/key')
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+//application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://halzis:dltk89@cluster0.x3oco.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected...'))
     .catch(err => console.log('err'))
 
 
-app.get('/', (req, res) => {res.send('Hello World!')})
+
+
+app.get('/', (req, res) => {res.send('Hello World! 아 해위 해윙')})
+
+
+app.post('/register',(req,res) => {
+    //회원가입시 필요한 정보들 client에서 가져오면
+    // 데이터베이스에 저장
+    const user = new User(req.body)
+    user.save((err, userInfo) => {
+        if(err) return res.json({ success: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
+})
 
 app.listen(port, () => {console.log(`Example app listening at http://localhost:${port}`)})
