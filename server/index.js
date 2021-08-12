35256f2b6b58a6badd40d14252e69fb880c90e40
halzis <halzis@kakao.com>
2021-08-12 18:14:47 +0900

    react 시작

--- server/index.js
+++ server/index.js
@@ -2,9 +2,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 const bodyParser = require('body-parser')
-const { User } = require('./models/User')
 const cookieParser = require('cookie-parser')
 const config = require('./config/key')
+const { auth } = require('./middleware/auth')
+const { User } = require('./models/User')
 
 //application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: true}));
@@ -26,7 +27,7 @@ mongoose.connect(config.mongoURI, {
 app.get('/', (req, res) => {res.send('Hello World! 아 해위 해윙')})
 
 // http://localhost:3000/register 로 회원가입 정보를 보낼 때
-app.post('/register', (req, res) => {
+app.post('/api/users/register', (req, res) => {
     // 회원가입시 필요한 정보들 client에서 가져오면
     // 데이터베이스에 저장
 
@@ -41,7 +42,7 @@ app.post('/register', (req, res) => {
 })
 
 // http://localhost:3000/login 으로 로그인 정보를 보낼 때
-app.post('/login', (req, res) => {
+app.post('/api/users/login', (req, res) => {
     // 전송된 JSON 형식의 정보(req)의 이메일을 DB에서 찾음
     User.findOne({ email: req.body.email }, (err, user) => {
         // DB에 로그인한 이메일이 없을 경우
@@ -70,7 +71,33 @@ app.post('/login', (req, res) => {
             })
         })
     })
+})
 
+// 미들웨어 auth
+app.get('api/users/auth', auth, (req,res) => {
+    // 여기까지 미들웨어를 통과해 왔다는 얘기는 Authentocation이 true라는 말.
+    res.status(200).json({
+        _id: req.user._id,
+        // role이 0이면 일반유저, 0이 아니면 어드민(관리자)
+        isAdmin: req.user.role === 0 ? false : true,
+        isAuth: true,
+        email: req.user.email,
+        name: req.user.name,
+        lastname: req.user.lastname,
+        role: req.user.role,
+        image: req.user.image
+    })
+})
+
+app.get('/api/users/logout', auth, (req, res) => {
+    User.findOneAndUpdate({ _id: req.user._id },
+        { token: '' },
+        (err, user) => {
+            if(err) return res.json({ success: false, err})
+            return res.status(200).send({
+                success: true
+            })
+        })
 })
 
 app.listen(port, () => {console.log(`Example app listening at http://localhost:${port}`)})
